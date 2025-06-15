d737c83ff801485cb3806058841c17d43175008f
nui <71966332+codenamenui@users.noreply.github.com>
2025-06-15 23:25:22 +0800

    bug fix

--- scenes/button.gd
+++ scenes/button.gd
@@ -1,4 +1,8 @@
 extends Button
 
+func _ready() -> void:
+	$".".pressed.connect(func(): AudioManager.play_sfx("button_click"))
+	$".".mouse_entered.connect(func(): AudioManager.play_ui_sound("button_hover"))
+	
 func _on_pressed() -> void:
 	SceneManager.transition_to_state(GameData.GameState.PAUSED)
